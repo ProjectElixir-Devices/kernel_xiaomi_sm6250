701479d77de71949abc5d90f5c06a84077a1d378
userariii <abhattacharjee717@gmail.com>
2024-02-05 17:31:50 +0000

    fixup! build error
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -1716,12 +1716,6 @@ static bool perf_event_validate_size(struct perf_event *event)
 				   group_leader->nr_siblings + 1) > 16*1024)
 		return false;
 
-	for_each_sibling_event(sibling, group_leader) {
-		if (__perf_event_read_size(sibling->attr.read_format,
-					   group_leader->nr_siblings + 1) > 16*1024)
-			return false;
-	}
-
 	return true;
 }
 
